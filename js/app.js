58ed24bbcc4320f32fe726fee23710a829db9bc1
Yaser <yaser.azem@otmail.com>
2020-05-08 13:51:39 +0300

    add random background option with local storage functionality

--- js/app.js
+++ js/app.js
@@ -51,13 +51,67 @@ colorsArr.forEach((li) => {
   });
 });
 
+// Change random background option
+
+let randomBckg = true;
+
+let bckgInterval;
+
+// Check local storage for background value
+let bckgLocalValue = localStorage.getItem('bckg-option');
+
+if (bckgLocalValue !== null) {
+  if (bckgLocalValue == 'yes') {
+    randomBckg = true;
+  } else {
+    randomBckg = false;
+  }
+} else {
+  console.log('No bckg value');
+}
+
+document.querySelectorAll('.random-background .btn').forEach((btn) => {
+  // remove active class
+  btn.addEventListener('click', (e) => {
+    e.target.parentElement.querySelectorAll('.active').forEach((el) => {
+      el.classList.remove('active');
+    });
+    btn.classList.add('active');
+
+    // deal with random background option
+    if (e.target.dataset.bckg == 'yes') {
+      randomBckg = true;
+      // console.log(randomBckg);
+      randomizeBckg();
+    } else {
+      randomBckg = false;
+      clearInterval(bckgInterval);
+    }
+
+    localStorage.setItem('bckg-option', e.target.dataset.bckg);
+  });
+
+  // Compare active btn with localStorage value
+  if (btn.dataset.bckg == localStorage.getItem('bckg-option')) {
+    btn.parentElement.querySelectorAll('.active').forEach((ele) => {
+      ele.classList.remove('active');
+    });
+    btn.classList.add('active');
+  }
+});
+
 //**//**// */ */
 // Landing Page
 //**//**// */ */
 let landingPage = document.querySelector('.landing-page');
 let imgesArr = ['1', '2', '3', '4', '5'];
-setInterval(() => {
-  let randNum = Math.floor(Math.random() * imgesArr.length);
-  landingPage.style.backgroundImage =
-    'url("images/' + imgesArr[randNum] + '.jpeg")';
-}, 10000);
+
+const randomizeBckg = () => {
+  if (randomBckg) {
+    bckgInterval = setInterval(() => {
+      let randNum = Math.floor(Math.random() * imgesArr.length);
+      landingPage.style.backgroundImage =
+        'url("images/' + imgesArr[randNum] + '.jpeg")';
+    }, 1000);
+  }
+};
